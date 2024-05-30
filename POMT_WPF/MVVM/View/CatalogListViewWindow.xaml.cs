0b1ffee870f3419658cee5b249b98a4679b5c755
NutellaTheHun <nbrilmayer3@gmail.com>
2024-05-30 15:09:46 -0400

    catalogView hooked up

--- POMT_WPF/MVVM/View/CatalogListViewWindow.xaml.cs
+++ POMT_WPF/MVVM/View/CatalogListViewWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using POMT_WPF.MVVM.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,8 @@ namespace POMT_WPF.MVVM.View
         public CatalogListViewWindow()
         {
             InitializeComponent();
+            CatalogListViewWindowModel model = new CatalogListViewWindowModel();
+            catalogListDataGrid.ItemsSource = model.Items;
         }
         private void CloseWindow_ButtonClick(object sender, RoutedEventArgs e)
         {
