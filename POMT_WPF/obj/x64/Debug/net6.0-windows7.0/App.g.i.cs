7ba27dd267c7163c22f057240b9f02efac911554
NutellaTheHun <nbrilmayer3@gmail.com>
2024-08-09 11:57:40 -0400

    fixed toggle editing

--- POMT_WPF/obj/x64/Debug/net6.0-windows7.0/App.g.i.cs
+++ POMT_WPF/obj/x64/Debug/net6.0-windows7.0/App.g.i.cs
@@ -65,7 +65,7 @@ namespace POMT_WPF {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/POMT_WPF;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/POMT_WPF;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- POMT_WPF/obj/x64/Debug/net6.0-windows7.0/MVVM/View/OrderView.g.i.cs
+++ POMT_WPF/obj/x64/Debug/net6.0-windows7.0/MVVM/View/OrderView.g.i.cs
@@ -127,7 +127,7 @@ namespace POMT_WPF.MVVM.View {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/POMT_WPF;V1.0.0.0;component/mvvm/view/orderview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/POMT_WPF;component/mvvm/view/orderview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\..\MVVM\View\OrderView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
