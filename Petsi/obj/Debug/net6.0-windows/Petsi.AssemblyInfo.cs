9065c9ccde2b1a3316ff3839067944799ddcae04
NutellaTheHun <nbrilmayer3@gmail.com>
2024-10-17 12:21:38 -0400

    updated tests
    templating improved, test cases updated

--- Petsi/obj/Debug/net6.0-windows/Petsi.AssemblyInfo.cs
+++ Petsi/obj/Debug/net6.0-windows/Petsi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Petsi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0aa83aa4c1f44efd89b07777a089d17a80bf92df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fac7c6ff598ffa316398b37674cf4726271ad8d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Petsi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Petsi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
