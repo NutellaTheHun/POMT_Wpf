c009fcde0666e8500ec5348ea1c67590761cfda0
NutellaTheHun <nbrilmayer3@gmail.com>
2024-11-19 16:25:18 -0500

    fixed backlistpie table builder
    removed takenbake and vegan takenbake check from MatchCatalogId(), was causing printing error for backlistpie report.

--- Petsi/obj/x64/Release/net6.0-windows/win-x64/Petsi.AssemblyInfo.cs
+++ Petsi/obj/x64/Release/net6.0-windows/win-x64/Petsi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Petsi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d691d504b470bec2af581cc3e7b9a86b717c59e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+205d8e35ce9159db70699c46df149a653583a9f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Petsi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Petsi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Square.Service/obj/x64/Release/net6.0-windows/Square.Service.AssemblyInfo.cs
+++ Square.Service/obj/x64/Release/net6.0-windows/Square.Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Square.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d691d504b470bec2af581cc3e7b9a86b717c59e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+205d8e35ce9159db70699c46df149a653583a9f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Square.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Square.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
