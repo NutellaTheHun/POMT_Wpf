79525442f02f6a451d6817429003e65220e05b03
NutellaTheHun <nbrilmayer3@gmail.com>
2024-12-06 15:22:55 -0500

    qs322

--- Petsi/obj/Debug/net6.0-windows/Petsi.AssemblyInfo.cs
+++ Petsi/obj/Debug/net6.0-windows/Petsi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Petsi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cce4bf1eb1d0403cdec4a348afa61dd8aaceb6af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d81af6af3f38614d33ccbc881c061fbe8045991")]
 [assembly: System.Reflection.AssemblyProductAttribute("Petsi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Petsi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
