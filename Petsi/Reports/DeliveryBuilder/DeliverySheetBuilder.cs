d5a5423d39a5dcc6775dbf023972cedf7d9ac143
NutellaTheHun <nbrilmayer3@gmail.com>
2024-12-17 16:48:16 -0500

    qs448

--- Petsi/Reports/DeliveryBuilder/DeliverySheetBuilder.cs
+++ Petsi/Reports/DeliveryBuilder/DeliverySheetBuilder.cs
@@ -34,10 +34,11 @@ namespace Petsi.Reports.DeliveryBuilder
                 IXLWorksheet page = deliveryPages.Worksheets.Add();
                 BuildDeliveryTable(0, 0, page, order);
                 //AddSeparation Line bottom of row 15
+                page.Range("A21:I21").Cells().Style.Border.SetBottomBorder(XLBorderStyleValues.Thin);
             }
             else
             {
-                BuildDeliveryTable(15, 1, deliveryPages.Worksheets.Last(), order);
+                BuildDeliveryTable(25, 1, deliveryPages.Worksheets.Last(), order);
             }
             
         }
@@ -49,71 +50,76 @@ namespace Petsi.Reports.DeliveryBuilder
             page.Range(GetLocalCell(1, 1), GetLocalCell(2, 3)).Merge();
             var imagePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "petsiDir\\images\\petsiLogo.png");
             var iamge = page.AddPicture(imagePath)
-                .MoveTo(GetLocalCell(1, 1));
+                .MoveTo(GetLocalCell(1, 1))
+                .Scale(0.05);
+
+            GetLocalCell(3, 3).Value = "Delivery";
+            GetLocalCell(3, 3).Style.Font.SetBold(true);
+            GetLocalCell(3, 3).Style.Font.SetFontSize(20);
 
             //4,1: Font 14, Bold, val="Day:"
-            GetLocalCell(4, 1).Value = "Day:";
-            GetLocalCell(4, 1).Style.Font.SetBold(true);
-            GetLocalCell(4, 1).Style.Font.SetFontSize(14);
+            GetLocalCell(6, 1).Value = "Day:";
+            GetLocalCell(6, 1).Style.Font.SetBold(true);
+            GetLocalCell(6, 1).Style.Font.SetFontSize(14);
 
             //4,2: font size 14, left aligned, order.fulfillmentDate.ToDatOfWeek()
-            GetLocalCell(4, 2).Value = DateTime.Parse(order.OrderDueDate).DayOfWeek.ToString();
-            GetLocalCell(4, 2).Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Left);
-            GetLocalCell(4, 2).Style.Font.SetFontSize(14);
+            GetLocalCell(6, 2).Value = DateTime.Parse(order.OrderDueDate).DayOfWeek.ToString();
+            GetLocalCell(6, 2).Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Left);
+            GetLocalCell(6, 2).Style.Font.SetFontSize(14);
 
             //5,1: Font 14, Bold, val="Date:"
-            GetLocalCell(5, 1).Value = "Date:";
-            GetLocalCell(5, 1).Style.Font.SetBold(true);
-            GetLocalCell(5, 1).Style.Font.SetFontSize(14);
-
-            //5,2: Font size 14, left aligned val=order.fulfillmentDate.ToShortDateString()
-            GetLocalCell(5, 2).Value = DateTime.Parse(order.OrderDueDate).ToShortDateString();
-            GetLocalCell(5, 2).Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Left);
-            GetLocalCell(5, 2).Style.Font.SetFontSize(14);
-
-            //7,1: Font size 14, bold, val="Order Contact:"
-            GetLocalCell(7, 1).Value = "Order Contact:";
+            GetLocalCell(7, 1).Value = "Date:";
             GetLocalCell(7, 1).Style.Font.SetBold(true);
             GetLocalCell(7, 1).Style.Font.SetFontSize(14);
 
-            //8,2: Font size 14, val=order.Name
-            GetLocalCell(8, 2).Value = order.Recipient;
-            GetLocalCell(8, 2).Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Left);
-            GetLocalCell(8, 2).Style.Font.SetFontSize(14);
+            //5,2: Font size 14, left aligned val=order.fulfillmentDate.ToShortDateString()
+            GetLocalCell(7, 2).Value = DateTime.Parse(order.OrderDueDate).ToShortDateString();
+            GetLocalCell(7, 2).Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Left);
+            GetLocalCell(7, 2).Style.Font.SetFontSize(14);
 
-            //7,5: Font size 14, bold, val="Delivery Address"
-            GetLocalCell(7, 5).Value = "Delivery Address";
-            GetLocalCell(7, 5).Style.Font.SetBold(true);
-            GetLocalCell(7, 5).Style.Font.SetFontSize(14);
-
-            //8,5: Font size 14, TextWrap, Merge to (10,7), val=order.DeliveryAddress
-            GetLocalCell(8, 5).Value = order.DeliveryAddress;
-            GetLocalCell(8, 5).Style.Font.SetFontSize(14);
-            GetLocalCell(8, 5).Style.Alignment.WrapText = true;
-            page.Range(GetLocalCell(8, 5), GetLocalCell(10, 7)).Merge();
-
-
-            //9,1: Font size 14, bold, val="Phone:"
-            GetLocalCell(9, 1).Value = "Phone:";
+            //7,1: Font size 14, bold, val="Order Contact:"
+            GetLocalCell(9, 1).Value = "Order Contact";
             GetLocalCell(9, 1).Style.Font.SetBold(true);
             GetLocalCell(9, 1).Style.Font.SetFontSize(14);
 
-            //10,2: font size 14, merge cell (10,3), left align, val=order.number
-            GetLocalCell(10, 2).Value = order.PhoneNumber;
-            GetLocalCell(10, 2).Style.Font.SetFontSize(14);
-            page.Range(GetLocalCell(10, 2), GetLocalCell(10, 3)).Merge();
+            //8,2: Font size 14, val=order.Name
+            GetLocalCell(10, 2).Value = order.Recipient;
             GetLocalCell(10, 2).Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Left);
+            GetLocalCell(10, 2).Style.Font.SetFontSize(14);
 
-            //11,1: Font size 14, bold, val="Instructions"
-            GetLocalCell(11, 1).Value = "Instructions:";
+            //7,5: Font size 14, bold, val="Delivery Address"
+            GetLocalCell(9, 5).Value = "Delivery Address";
+            GetLocalCell(9, 5).Style.Font.SetBold(true);
+            GetLocalCell(9, 5).Style.Font.SetFontSize(14);
+
+            //8,5: Font size 14, TextWrap, Merge to (10,7), val=order.DeliveryAddress
+            GetLocalCell(10, 5).Value = order.DeliveryAddress;
+            GetLocalCell(10, 5).Style.Font.SetFontSize(14);
+            GetLocalCell(10, 5).Style.Alignment.WrapText = true;
+            page.Range(GetLocalCell(10, 5), GetLocalCell(12, 7)).Merge();
+
+
+            //9,1: Font size 14, bold, val="Phone:"
+            GetLocalCell(11, 1).Value = "Phone";
             GetLocalCell(11, 1).Style.Font.SetBold(true);
             GetLocalCell(11, 1).Style.Font.SetFontSize(14);
 
+            //10,2: font size 14, merge cell (10,3), left align, val=order.number
+            GetLocalCell(12, 2).Value = order.PhoneNumber;
+            GetLocalCell(12, 2).Style.Font.SetFontSize(14);
+            page.Range(GetLocalCell(12, 2), GetLocalCell(12, 3)).Merge();
+            GetLocalCell(12, 2).Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Left);
+
+            //11,1: Font size 14, bold, val="Instructions"
+            GetLocalCell(13, 1).Value = "Instructions";
+            GetLocalCell(13, 1).Style.Font.SetBold(true);
+            GetLocalCell(13, 1).Style.Font.SetFontSize(14);
+
             //12,1: Font size 14, merge (14,7), val=order.notes?
-            GetLocalCell(12, 1).Value = order.Note;
-            GetLocalCell(12, 1).Style.Font.SetFontSize(14);
-            GetLocalCell(12, 1).Style.Alignment.WrapText = true;
-            page.Range(GetLocalCell(12, 1), GetLocalCell(14, 7)).Merge();
+            GetLocalCell(14, 1).Value = order.Note;
+            GetLocalCell(14, 1).Style.Font.SetFontSize(14);
+            GetLocalCell(14, 1).Style.Alignment.WrapText = true;
+            page.Range(GetLocalCell(14, 1), GetLocalCell(16, 7)).Merge();
 
             IXLCell GetLocalCell(int row, int col) 
             {
